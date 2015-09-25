ee7ce20021ec4bb2b16b8a1d2a35290732805b72
Batiatari Kaze <batiatari.dono@gmail.com>
2015-09-25 09:21:25 +0300

    Правильно выводит

--- main.cpp
+++ main.cpp
@@ -15,12 +15,14 @@ using namespace std;
 
 FILE *fp;
 short mas[100];
+short sum[100];
+short n;
 
 int main(int argc, char *argv[]) {
 	
 	fp = fopen(argv[1], "r");
     char buffer[128];
-	short n, temp;
+	short temp;
     short i, j, scanresult;
     freopen(argv[1], "r", stdin);
 	
@@ -47,19 +49,23 @@ L:
 		++i;
 		scanresult = scanf("%i", &mas[i]);
 	}
-
-	cout << n << ';';
-	for(int j = 0; j < i-1; ++j)
-		printf("%i ", mas[j]);
-	
-	n = mas[i-1];
-	if(scanf(";%i", &mas[0]) == 1) {
-		i = 1;
+	if(scanf(";%i", &temp) == 1) {
+		cout << n << ';';
+		for(int j = 0; j < i-1; ++j)
+			printf("%i ", mas[j]);
 		cout << endl;
+		
+		
+		
+		n = mas[i-1];
+		i = 1;
+		mas[0] = temp;
 		goto L;
 	}
-	else {
-		cout << n;
+	else {	
+		cout << n << ';';
+		for(int j = 0; j < i; ++j)
+			printf("%i ", mas[j]);
 		cout << endl;
 	}
 	return 0;
